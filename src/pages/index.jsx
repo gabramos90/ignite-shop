63b6ac5cc295ad0002e3c9e3440ca0505294e9e0
Gabriel Benedito <gb953132@gmail.com>
2022-11-30 04:05:47 -0300

    server side props added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ import camiseta3 from '../assets/camisetas/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -19,6 +19,8 @@ export default function Home() {
 
   return(
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+
       <Product className='keen-slider__slide'>
         <Image src={camiseta1} width={520} height={480} alt=''/>
 
@@ -57,3 +59,16 @@ export default function Home() {
     </HomeContainer>
   )
 }
+
+// nem toda requisição sera feita no getServer,  pois isso deixa o site lento
+
+export const getServerSideProps = async () => { // essa função carrega todos os dados de uma vez só, então nao vai existir estado de loading
+  await new Promise(resolver => setTimeout(resolver, 2000)) // simular delay do navegador (hack)
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
+}
+// só vamos usar essa opção para informaçoes que precisam estar em tela assim que a aplicação fo carregada, SOMENTE APLICAÇÕES CRUCIAIS! => . PARA INDEXADROES, CROWLER E BOTS.
